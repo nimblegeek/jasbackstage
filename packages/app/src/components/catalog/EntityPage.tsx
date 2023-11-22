4039cd05b530d6a95e78f1b7b5ede388df9bf782
Jonas A Sihlén <jonasasihlen@gmail.com>
2023-11-22 23:18:43 +0100

    Config for k8s plugin

--- packages/app/src/components/catalog/EntityPage.tsx
+++ packages/app/src/components/catalog/EntityPage.tsx
@@ -58,6 +58,10 @@ import {
 import { TechDocsAddons } from '@backstage/plugin-techdocs-react';
 import { ReportIssue } from '@backstage/plugin-techdocs-module-addons-contrib';
 
+import { EntityKubernetesContent } from "@backstage/plugin-kubernetes";
+
+
+
 const techdocsContent = (
   <EntityTechdocsContent>
     <TechDocsAddons>
